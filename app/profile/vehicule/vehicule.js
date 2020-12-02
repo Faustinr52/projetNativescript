aa25e10cdba316d035e9e7c44b2507dff4ec6a4d
faustinr52@gmail.com <rurangirwafaustin8@mgail.com>
2020-12-02 11:42:13 +0100

    styles

--- app/profile/vehicule/vehicule.js
+++ app/profile/vehicule/vehicule.js
@@ -1,6 +1,6 @@
 exports.retourProfile = args => {
     const button = args.object
     const page = button.page
-    page.frame.navigate('~/home/profile/vehicule/vehicule');
+    page.frame.navigate('profile/vehicule/vehicule');
     // page.frame.goBack();
 }
